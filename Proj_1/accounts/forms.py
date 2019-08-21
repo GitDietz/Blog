9efc58e417371df2bb8c916817f0801f0d27d7d1
Getafix <thierrydna@me.com>
2019-08-21 20:10:16 +1000

    part done
    almost there

--- Proj_1/accounts/forms.py
+++ Proj_1/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth import (
     authenticate,
@@ -14,15 +15,23 @@ class UserLoginForm(forms.Form):
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
+
         # possible to use
         # user_qs = User.objects.filter(username=username)
         # if user_qs.count ==1:
         #     user = user_qs.first()
-        if not user:
-            raise ValidationError("This user does not exist")
-        if not user.check_password(password):
-            raise ValidationError("The password is incorrect")
-        if not user.is_active:
-            raise ValidationError("This user is not active")
+        if username and password:
+            known_user = User.objects.filter(username=username).exists
+            user = authenticate(username=username, password=password)
+            if not known_user:
+                print('unknown user')
+                raise ValidationError("This user does not exist")
+            else:
+                print('passw fault')
+                raise ValidationError("The password is incorrect")
+                # if not user.check_password(password):
+            if user:
+                print('knwon user')
+                if not user.is_active:
+                    raise ValidationError("This user is not active")
         return super(UserLoginForm, self).clean(*args,**kwargs)
--- Proj_1/accounts/views.py
+++ Proj_1/accounts/views.py
@@ -15,9 +15,13 @@ def login_view(request):
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        print('Is the user ok? ' + str(request.user.is_authenticated()))
+
     context = {'form':form,
                'title':title}
-    return render(request, "login_form.html", context = context)
+    return render(request, "login_form.html", context=context)
 
 def register_view(request):
     return render(request, "form.html", {})
